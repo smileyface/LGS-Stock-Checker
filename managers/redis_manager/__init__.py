f8555cc3d776002af29c959df82b6a774316d761
smileyface <kasonbennett65@gmail.com>
2025-02-24 18:04:07 -0700

    [Feature]: RQ worker can now emit socketIO

--- managers/redis_manager/__init__.py
+++ managers/redis_manager/__init__.py
@@ -1,8 +1,9 @@
 from .redis_manager import RedisManager, REDIS_URL
-import redis_function_register
+from .redis_function_register import register_redis_function
 
 # Singleton instance
 redis_manager = RedisManager()
+register_redis_function()
 
 
 __all__ = ["redis_manager", "REDIS_URL"]

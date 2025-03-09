d1fdf6dbb3bbb445a93afda555280a4ad5b9389f
smileyface <kasonbennett65@gmail.com>
2025-03-09 00:09:48 -0700

    [Feature]: Send Add Card

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -162,7 +162,7 @@ document.getElementById("saveCardButton").addEventListener("click", function () 
         return;
     }
 
-    let amount = document.getElementById("amount")
+    let amount = document.getElementById("amount").value
 
     let cardSpecs = {
         set_code: document.getElementById("setCode")?.value || "Unknown",

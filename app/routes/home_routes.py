8182527473cadbe0af071d34bd7f9561b75101eb
smileyface <kasonbennett65@gmail.com>
2025-03-15 17:14:18 -0700

    [Fix]: Remove dead code for tasks. Call store scraping on cache miss

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -1,6 +1,5 @@
 from flask import session, Blueprint, render_template, redirect, url_for
 
-from managers.availability_manager import get_card_availability
 from managers.socket_manager.socket_manager import log_and_emit
 
 home_bp = Blueprint("home_bp", __name__)

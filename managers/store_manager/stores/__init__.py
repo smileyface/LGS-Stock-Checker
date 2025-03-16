92b939178e48d3a8f63d9592f3b8145e862ccd43
smileyface <kasonbennett65@gmail.com>
2025-03-15 17:24:35 -0700

    [Fix]: Slug Fix

--- managers/store_manager/stores/__init__.py
+++ managers/store_manager/stores/__init__.py
@@ -4,8 +4,8 @@ STORE_REGISTRY = {
     "authority_games_mesa_az": Authority_Games_Mesa_Arizona()
 }
 
-def store_list(slug_list):
+def store_list(store_list):
     list_of_objects = []
-    for x in slug_list:
-        list_of_objects.append(STORE_REGISTRY[x])
+    for x in store_list:
+        list_of_objects.append(STORE_REGISTRY[x.slug])
     return list_of_objects

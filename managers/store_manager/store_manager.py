32baa67bffaedf61244604c68d009a9858441634
smileyface <kasonbennett65@gmail.com>
2025-03-17 21:40:00 -0700

    [Fix]: Slug Fix

--- managers/store_manager/store_manager.py
+++ managers/store_manager/store_manager.py
@@ -42,9 +42,9 @@ def scrape_store_availability(card_name, username):
     logger.info(f"📂 Found {len(store_classes)} matching store scrapers.")
 
     for store_class in store_classes:
-        store_name = store_class.slug
         try:
             store_instance = store_class()  # Instantiate store class
+            store_name = store_class.slug
             logger.info(f"🔍 Scraping '{store_name}' for card: {card_name}")
 
             store_listings = store_instance.check_store_availability(card_name)

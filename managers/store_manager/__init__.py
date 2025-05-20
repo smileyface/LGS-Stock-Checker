f62c0b2091b5bcab2a0dd15e14902f35d37c137f
smileyface <kasonbennett65@gmail.com>
2025-05-20 14:24:56 -0700

    [Fix]: Fix the issue with the availability cache not updating correctly. The cache should be updated whenever availability data is saved. The current implementation only updates the cache when the availability data is loaded.

--- managers/store_manager/__init__.py
+++ managers/store_manager/__init__.py
@@ -1,5 +1,5 @@
-from .store_manager import save_store_availability, scrape_store_availability
+from .store_manager import scrape_store_availability
 from .stores import store_list
 from .stores.store import Store
 
-__all__ = ["save_store_availability", "scrape_store_availability", "store_list", "Store"]
+__all__ = ["scrape_store_availability", "store_list", "Store"]

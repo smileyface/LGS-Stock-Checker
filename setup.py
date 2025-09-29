8588534912a25666ac07a68934045e9eeb6283b9
smileyface <kasonbennett65@gmail.com>
2025-09-29 13:12:16 -0700

    The changes configure the backend to use Gunicorn with a paste configuration file for deployment, installing the application in editable mode and updating the Dockerfile and docker-compose.yml to reflect these changes.

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name='LGS_Stock_Backend',
     version='0.1.0',
     packages=find_packages(),
+    entry_points={
+        'paste.app_factory': [
+            'main = run:create_app',
+        ],
+    }
 )

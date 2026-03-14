e75b18401967879a214d3e6fd3a13963986a332d
Kason <kasonbennett65@gmail.com>
2026-03-14 12:58:02 -0700

    missed one

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginVue from 'eslint-plugin-vue';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default tseslint.config(
   // 1. Ignore build output and node_modules
